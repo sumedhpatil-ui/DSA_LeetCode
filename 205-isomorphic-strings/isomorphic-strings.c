40ce2658377ee0b856d71494a533dd3f13f6f2ae
sumedhpatil <sumedhpatil700@gmail.com>
2026-06-15 18:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (59.25%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.c
@@ -0,0 +1,26 @@
+bool isIsomorphic(char* s, char* t) {
+    int n = strlen(s);
+    if(strlen(t) != n)
+        return false;
+    int mapST[256], mapTS[256];
+    for(int i = 0; i < 256; i++)
+    {
+        mapTS[i] = -1;
+        mapST[i] = -1;
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        unsigned char c1 = s[i];
+        unsigned char c2 = t[i];
+
+        if(mapST[c1] == -1 && mapTS[c2] == -1)
+        {
+            mapST[c1] = c2;
+            mapTS[c2] = c1;
+        }
+        else if(mapST[c1] != c2 || mapTS[c2] != c1)
+            return false;
+    }
+    return true;
+}
