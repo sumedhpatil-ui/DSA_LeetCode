d32d79ce188a5635e7c043e9dad6955ac9e68502
sumedhpatil <sumedhpatil700@gmail.com>
2026-06-15 17:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (57.14%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    string convertToTitle(int columnNumber) {
+
+        string ans;
+
+        while(columnNumber > 0)
+        {
+            columnNumber--;
+
+            char ch = 'A' + (columnNumber % 26);
+
+            ans += ch;
+
+            columnNumber /= 26;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
