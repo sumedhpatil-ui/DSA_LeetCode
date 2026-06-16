a2d0b8da4e8d5a27f3fc6cdda0d9756f0c2529fc
sumedhpatil <sumedhpatil700@gmail.com>
2026-06-16 22:21:41 +0530

    Time: 9 ms (20.08%) | Memory: 40.1 MB (19.70%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -4,22 +4,19 @@ public:
         string ans;
         for(auto &it : s)
         {
-            if(ans.size() != 0 && it == '*')
+            if(it >= 97 && it <= 122)
+                ans = ans + it;
+
+            else if(it == '*')
             {
-                ans.pop_back();
+                if(!ans.empty())
+                    ans.pop_back();
             }
             else if(it == '#')
-            {
                 ans += ans;
-            }
+
             else if(it == '%')
-            {
                 reverse(ans.begin(), ans.end());
-            }
-            else if(it >= 97 && it <= 122)
-            {
-                ans = ans + it;
-            }
         }
         return ans;
     }
