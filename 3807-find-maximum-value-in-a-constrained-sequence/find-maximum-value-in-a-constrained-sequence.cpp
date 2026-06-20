fdc0383f1d1c9e21a1de2bf2c093543b0445652d
sumedhpatil <sumedhpatil700@gmail.com>
2026-06-20 21:06:37 +0530

    Time: 99 ms (14.29%) | Memory: 302 MB (14.78%) - LeetSync

---
+++ 3807-find-maximum-value-in-a-constrained-sequence/find-maximum-value-in-a-constrained-sequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxVal(int n, vector<vector<int>>& restrictions, vector<int>& diff) {
+        vector<int> heights(n, 1e9);
+        heights[0] = 0; 
+        for(auto it : restrictions)
+        {
+            heights[it[0]] = min(heights[it[0]], it[1]);
+        }
+        
+        for(int i = 0; i < n-1 ; i++)
+        {
+            heights[i + 1] = min(heights[i+1], heights[i] + diff[i]);
+        }
+        for(int j = n-2; j >= 0; j--)
+        {
+            heights[j] = min(heights[j], heights[j+1] + diff[j]);
+        }
+
+        int ans = 0;
+        for(int i : heights)
+            ans = max(ans, i);
+        return ans;
+    }
+};
