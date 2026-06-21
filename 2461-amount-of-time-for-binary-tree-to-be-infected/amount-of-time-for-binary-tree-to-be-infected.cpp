44042a8fcf7cc7611607477401e91f089803a254
sumedhpatil <sumedhpatil700@gmail.com>
2026-06-21 15:49:30 +0530

    Time: 8 ms (93.82%) | Memory: 129.5 MB (93.49%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -11,51 +11,31 @@
  */
 class Solution {
 public:
-    void build(TreeNode* root, unordered_map<int, vector<int>>& adj)
+    int dfs(TreeNode* root, int start, int* ans)
     {
         if(root == NULL)
-            return ;
-        int rootVal = root->val;
-        int v;
-        if(root->left)
-        {   
-            v = root->left->val;
-            adj[rootVal].push_back(v);
-            adj[v].push_back(rootVal);
-            build(root->left, adj);
-        }
-        if(root->right)
+            return 0;
+        int leftHt = dfs(root->left, start, ans);
+        int rightHt = dfs(root->right, start, ans);
+
+        if(root->val == start)
         {
-            v = root->right->val;
-            adj[rootVal].push_back(v);
-            adj[v].push_back(rootVal);
-            build(root->right, adj);
+            *ans = max(leftHt, rightHt);
+            return -1;
+        }
+        else if(leftHt >= 0 && rightHt >= 0)
+            return max(leftHt, rightHt) + 1;
+        else
+        {
+            *ans = max(*ans, abs(leftHt) + abs(rightHt));
+            return min(leftHt, rightHt) - 1;
         }
     }
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<int, vector<int>> adj;
-        build(root, adj);
-        unordered_map<int, bool> vis;
-        queue<pair<int, int>> q;
-        int ans  = 0;
-        q.push({start, 0});
-        vis[start] = true;
-        while(!q.empty())
-        {
-            int node = q.front().first;
-            int count = q.front().second;
-            q.pop();
-
-            for(auto adjNode : adj[node])
-            {
-                if(!vis[adjNode])
-                {
-                    vis[adjNode] = true;
-                    q.push({adjNode, count + 1});
-                }
-                ans = max(ans, count);
-            }
-        }
+        if(root == NULL)
+            return 0;
+        int ans = 0;
+        dfs(root, start, &ans);
         return ans;
     }
 };
