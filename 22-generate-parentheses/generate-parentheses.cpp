f9989035d4f63e91edf8526f0bc6e305372dad28
sumedhpatil <sumedhpatil700@gmail.com>
2026-06-23 12:15:53 +0530

    Time: 4 ms (33.16%) | Memory: 15.5 MB (65.25%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,49 +1,20 @@
 class Solution {
 public:
-    bool isValid(string& s)
+    void generate(int open, int close, string curr, vector<string>& ans)
     {
-        stack<char> st;
-        for(char c : s)
-        {
-            if(c == '(')
-            {
-                st.push('(');
-            }
-            else
-            {
-                if(st.empty())
-                {
-                    return false;
-                }
-                st.pop();
-            }
-        }
-        return st.empty();
-    }
-    void generate(int n, string curr, vector<string>& ans)
-    {
-        int len = curr.size();
-        if(len == 2*n)
+        if(open == 0 && close == 0)
         {
             ans.push_back(curr);
             return;
         }
-        generate(n, curr + '(', ans);
-        generate(n, curr + ')', ans);
+        if(open > 0)
+            generate(open - 1, close, curr + "(", ans);
+        if(close > open)
+            generate(open, close - 1, curr + ")", ans);
     }
-
     vector<string> generateParenthesis(int n) {
-        vector<string> temp;
-        generate(n, "", temp);
-
         vector<string> ans;
-        for(auto it : temp)
-        {
-            if(isValid(it))
-            {
-                ans.push_back(it);
-            }
-        } 
-        return ans;   
+        generate(n, n, "", ans);
+        return ans;
     }
 };
