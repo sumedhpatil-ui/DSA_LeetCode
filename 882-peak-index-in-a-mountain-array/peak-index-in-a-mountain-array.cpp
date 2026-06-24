09eaa10896c47303058cf0a0790d0524ff7cf849
sumedhpatil <sumedhpatil700@gmail.com>
2026-06-24 10:43:42 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (78.41%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int i = 0;
+        int n = arr.size();
+        while(i < n-1 && arr[i] < arr[i+1])
+        {
+            i++;
+        }
+        return i;
+    }
+};
