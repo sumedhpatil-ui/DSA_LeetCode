195b8e1f50a7e6381854639acd045156f61322f5
sumedhpatil <sumedhpatil700@gmail.com>
2026-07-04 18:05:28 +0530

    Time: 100 ms (34.20%) | Memory: 130.6 MB (81.43%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,68 @@
+class DisjointSet{
+public:
+    vector<int> parent;
+    vector<int> size;
+    DisjointSet(int n)
+    {
+        parent.resize(n);
+        size.resize(n);
+        for(int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int upar_u = uPar(u);
+        int upar_v = uPar(v);
+
+        if(upar_u == upar_v)
+            return;
+        if(size[upar_u] > size[upar_v])
+        {
+            parent[upar_v] = upar_u;
+            size[upar_u] += size[upar_v];
+        }
+        else
+        {
+            parent[upar_u] = upar_v;
+            size[upar_v] += size[upar_u];
+        }
+    }
+
+    int uPar(int u)
+    {
+        if(parent[u] == u)
+            return u;
+        return parent[u] = uPar(parent[u]);
+    }
+};
+
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        DisjointSet ds(n + 1);
+        int ans = INT_MAX;
+        for(auto it : roads)
+        {
+            ds.unionBySize(it[0], it[1]);
+        }
+
+        if(ds.uPar(1) != ds.uPar(n))
+            return -1;
+
+        for(auto it : roads)
+        {
+            int u = it[0];
+            int v = it[1]; 
+            int w = it[2];
+            if(ds.uPar(u) == ds.uPar(1))
+            {
+                ans = min(ans, w);
+            }
+        }
+        return ans;
+    }
+};
