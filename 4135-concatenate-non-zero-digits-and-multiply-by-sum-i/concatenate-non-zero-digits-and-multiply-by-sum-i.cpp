e058ab86a225290c05d15b12058c0ca4d10c83a0
sumedhpatil <sumedhpatil700@gmail.com>
2026-07-07 09:42:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (98.81%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        stack<int> st;
         long long sum = 0;
-        while(n != 0)
-        {
-            int dig = n % 10;
-            if(dig != 0){
-                sum += dig;
-                st.push(dig);
+        long long num = 0;
+        long long place = 1;
+
+        while (n > 0) {
+            int d = n % 10;
+            if (d != 0) {
+                sum += d;
+                num = d * place + num;
+                place *= 10;
             }
-            n = n / 10;
+            n /= 10;
         }
 
-        long long ans = 0;
-        while(!st.empty())
-        {
-            int dig = st.top();
-            st.pop();
-            ans = ans * 10 + dig;
-        }
-        return ans * sum;
+        return num * sum;
     }
 };
