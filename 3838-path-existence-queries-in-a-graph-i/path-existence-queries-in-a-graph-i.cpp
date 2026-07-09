a8d260af67ee99661cf1bc6a3d6ecc8065f573be
sumedhpatil <sumedhpatil700@gmail.com>
2026-07-09 12:54:11 +0530

    Time: 4 ms (93.70%) | Memory: 224.1 MB (66.03%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -1,70 +1,18 @@
-class DisjointSet{
-public:
-    vector<int> size;
-    vector<int> parent;
-    DisjointSet(int n)
-    {
-        size.resize(n);
-        parent.resize(n);
-        for(int i = 0; i < n; i++)
-        {
-            size[i] = 1;
-            parent[i] = i;
-        }
-    }
-
-    int findUpar(int u)
-    {
-        if(parent[u] == u)
-            return u;
-        return parent[u] = findUpar(parent[u]);
-    }
-
-    void unionBySize(int u, int v)
-    {
-        int u_par = findUpar(u);
-        int v_par = findUpar(v);
-
-        if(u_par == v_par)
-            return;
-        if(size[u_par] < size[v_par])
-        {
-            parent[u_par] = v_par;
-            size[v_par] += size[u_par];
-        }
-        else
-        {
-            parent[v_par] = u_par;
-            size[u_par] += size[v_par];
-        }
-    }
-};
-
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        DisjointSet ds(n);
+        vector<int> group(n);
+        int id = 0;
         for(int i = 1; i < n; i++)
         {
-            if(nums[i] - nums[i-1] <= maxDiff)
-            {
-                ds.unionBySize(i, i-1);
-            }
+            if(nums[i] - nums[i-1] > maxDiff)
+                id++;
+            group[i] = id; 
         }
 
         vector<bool> ans;
-        for(auto it : queries)
-        {
-            int u = it[0];
-            int v = it[1];
-
-            if(ds.findUpar(u) == ds.findUpar(v))
-            {
-                ans.push_back(true);
-            }
-            else
-                ans.push_back(false);
-        }
+        for (auto &q : queries)
+            ans.push_back(group[q[0]] == group[q[1]]);
         return ans;
     }
 };
