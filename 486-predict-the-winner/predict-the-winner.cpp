e34c4ac2f46655923245d6d696a2c1cdd3495225
sumedhpatil <sumedhpatil700@gmail.com>
2026-08-01 11:09:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (16.44%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(vector<int>& nums, int l, int r) {
+        if (l == r)
+            return nums[l];
+
+        if (dp[l][r] != INT_MIN)
+            return dp[l][r];
+
+        int takeLeft = nums[l] - solve(nums, l + 1, r);
+        int takeRight = nums[r] - solve(nums, l, r - 1);
+
+        return dp[l][r] = max(takeLeft, takeRight);
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        dp.assign(n, vector<int>(n, INT_MIN));
+
+        return solve(nums, 0, n - 1) >= 0;
+    }
+};
